eb9dc88e28b034581d5153c0cc2b8676014aa89e
Peter Centellini <peter.centellini@redesajn.com>
2019-10-26 18:12:45 +0200

    Initial Commit

---
+++ FuelLog/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  FuelLog
+//
+//  Created by Peter Centellini on 2019-10-26.
+//  Copyright © 2019 Redesajn Interactive Solutions. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FuelLog/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  FuelLog
+//
+//  Created by Peter Centellini on 2019-10-26.
+//  Copyright © 2019 Redesajn Interactive Solutions. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
